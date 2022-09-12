91d8f40299009aee8d7965eb4ec1d2188d7a0988
Nataly2022 <107841711+Nataly2022@users.noreply.github.com>
2022-09-12 17:47:24 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+
+Token = '5611175766:AAEXTyKNWzkfpkEdNTasgHay7wZD_wAyUTI'
+
+keys = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUR',
+}
